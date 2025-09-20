1188ebf8bca0f06201ee5570fd1486fe22488604
dobr1337 <nikitapavlov03209@gmail.com>
2025-09-20 12:43:47 +0300

    new

--- app/services/panels.py
+++ app/services/panels.py
@@ -13,9 +13,14 @@ class PanelService:
         urls: List[str] = []
         for p in panels:
             client = XUIPanelClient(p.base_url, p.username, p.password, p.domain)
-            await client.create_or_update_user(uid, expires)
-            urls.append(await client.subscription_url(uid))
-            await client.close()
+            try:
+                inbounds = await client.list_inbounds()
+                ids = [ib.get("id") or ib.get("Id") for ib in inbounds if ib]
+                for inbound_id in filter(lambda x: isinstance(x, int), ids):
+                    await client.add_client_to_inbound(inbound_id, uid, expires)
+                urls.append(await client.subscription_url(uid))
+            finally:
+                await client.close()
         return urls
 
     def aggregate_subscription(self, uid: str, urls: List[str]) -> str:
